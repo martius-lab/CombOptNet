d27f4b09a77d28c9e1f0459554a9657a30e12177
Anselm <anselm.paulus@tuebingen.mpg.de>
2022-04-22 17:53:39 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,15 @@
 from distutils.core import setup
 
 setup(
-    name="comboptnet_module",
+    name="constraint_handler",
     version="1.0",
-    description="Differentiable ILP solver module from 'CombOptNet: Fit the Right NP-Hard Problem by Learning Integer Programming Constraints' (ICML 2021, Paulus et al.)",
+    description="Handler for initializing a learnable set of constraints.",
     author="Anselm Paulus",
     author_email="anselm.paulus@tuebingen.mpg.de",
     url=None,
-    packages=["comboptnet_module"],
+    packages=["constraint_handler"],
     install_requires=[
         "torch>=1.5.0",
-        "numpy",
-        "ray",
-        "jax>=0.2.8",
-        "jaxlib>=0.1.58",
-        "gurobipy",
+        "numpy"
     ],
 )
